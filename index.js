2b930b265119e35d208cd4504d085479c4d688f1
jakeu222 <81273955+jakeu222@users.noreply.github.com>
2024-02-13 11:08:39 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+
+document.querySelector(".list a").style.color= "red"
+var listItem = document.querySelectorAll(".list")
+newText= listItem[2]
+newText.style.color = "purple"
+newText.innerHTML = "purple"
+newerText = listItem[1]
+newerText.innerHTML = "LDFGHDSALFHG"
+
+document.querySelector(".btn").style.backgroundColor = "green"
+document.querySelector("button").classList.add("gangster")
+document.querySelector("button").classList.remove("gangster")
+document.querySelector("button").classList.toggle("gangster")
+document.querySelector("button").classList.toggle("gangster")
+document.querySelector("h1").classList.add("huge")
+document.querySelector("h1").innerHTML = "<em>sup</em>"
+document.querySelector("a").setAttribute("href" , "https://www.bing.com")
